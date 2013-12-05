b6ef5f0bea276e1d3b90c9fdd857a2fb2ce3ea5d
Johan Euphrosine <proppy@google.com>
2013-12-05 01:40:37 -0800

    make_busybox: add docker

--- rootfs/make_busybox.sh
+++ rootfs/make_busybox.sh
@@ -4,8 +4,8 @@ TMPDIR=$(mktemp -d)
 pushd $TMPDIR
 function defer {
   popd
-  cp $TMPDIR/rootfs.cpio.gz initrd.gz
-  file initrd.gz
+  cp $TMPDIR/rootfs.cpio.gz busybox-initrd.gz
+  file busybox-initrd.gz
 }
 trap defer EXIT
 
@@ -13,6 +13,10 @@ wget http://busybox.net/downloads/binaries/latest/busybox-x86_64
 chmod +x busybox-x86_64
 mkdir -p rootfs/bin
 ./busybox-x86_64 --install rootfs/bin
+
+wget https://get.docker.io/builds/Linux/x86_64/docker-latest -O  rootfs/bin/docker
+chmod +x rootfs/bin/docker
+
 cd rootfs
 mkdir dev proc sys tmp
 mknod dev/console c 5 1
